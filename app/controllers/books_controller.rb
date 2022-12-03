deb37987df19ad1b91d050c1f3567772cdf97186
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-12-03 00:28:52 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,11 +24,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -37,7 +35,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -47,6 +44,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
   def ensure_correct_user
     @book = Book.find(params[:id])
     unless @book.user == current_user
