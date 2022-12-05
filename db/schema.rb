8caa6d915e6b6b5e7de83db025d2e012d28c70b9
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-12-05 04:23:54 +0000

    [Finish]

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2022_12_03_054908) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
-    t.integer "followed"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
