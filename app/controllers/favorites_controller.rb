8da5386e0336da462751a0fa08ba453d176798d1
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-12-07 04:16:07 +0000

    [Finish]応用課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    # app/views/favorites/create.js.erbを参照させる
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    # app/views/favorites/destroy.js.erbを参照する
   end
 end
